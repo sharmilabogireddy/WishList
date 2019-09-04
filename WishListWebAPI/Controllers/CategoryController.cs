f815f4b2b31b737441531d74ec6a04e84ff63b76
Sharmila <sharmila.bogireddy@gmail.com>
2019-09-04 17:11:09 +1000

    First commit

---
+++ WishListWebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WishListWebAPI.Models;
+using System.Data.Entity;
+
+namespace WishListWebAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        WishListCategoryDBEntities wishList = new WishListCategoryDBEntities();
+        //api/Category
+        [HttpGet]
+        public IQueryable<Category> GetAllCategories()
+        {
+            return wishList.Categories;
+        }
+        //api/Category/1
+        [HttpGet]
+        public Category GetCategoryById(int id)
+        {
+            Category category = wishList.Categories.Find(id);
+            return category;
+        }
+        //api/Category/1
+        [HttpPut]
+        public void UpdateCategory(int id, Category category)
+        {
+            wishList.Entry(category).State = EntityState.Modified;
+            wishList.SaveChanges();
+        }
+
+        //api/Category/1
+        [HttpDelete]
+        public void DeleteCategoryById(int id)
+        {
+            Category category = wishList.Categories.Find(id);
+            wishList.Categories.Remove(category);
+            wishList.SaveChanges();
+        }
+        //api/Category
+        [HttpPost]
+        public void CreateNewCategory(Category category)
+        {
+            wishList.Categories.Add(category);
+            wishList.SaveChanges();
+        }
+    }
+}
---
+++ WishListWebAPI/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WishListWebAPI.Models;
+using System.Data.Entity;
+
+namespace WishListWebAPI.Controllers
+{
+    public class RoleController : ApiController
+    {
+        WishListDBEntities wishList = new WishListDBEntities();
+        //../api/Role
+        [HttpGet]
+        public IQueryable<Role> GetAllRoles()
+        {
+            return wishList.Roles;
+        }
+        //../api/Role/1
+        [HttpGet]
+        public Role GetRoleById(int id)
+        {
+            Role role = wishList.Roles.Find(id);
+            return role;
+        }
+        //api/Role/1
+        [HttpPut]
+        public void UpdateRole(int id, Role role)
+        {
+            wishList.Entry(role).State = EntityState.Modified;
+            wishList.SaveChanges();
+        }
+
+        //api/Role/1
+        [HttpDelete]
+        public void DeleteRoleById(int id)
+        {
+            Role role = wishList.Roles.Find(id);
+            wishList.Roles.Remove(role);
+            wishList.SaveChanges();
+        }
+        //api/Role
+        [HttpPost]
+        public void CreateNewRole(Role role)
+        {
+            wishList.Roles.Add(role);
+            wishList.SaveChanges();
+        }
+    }
+}
